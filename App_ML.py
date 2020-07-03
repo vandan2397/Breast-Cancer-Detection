a1a9f619cdd0b9a57839eb718e88695823276805
Vandan pandya <55615788+vandan2397@users.noreply.github.com>
2020-07-04 02:17:40 +0530

    Update App_ML.py

--- App_ML.py
+++ App_ML.py
@@ -15,7 +15,7 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-	int_features=[int(x) for x in request.form.values()]
+	int_features=[float(x) for x in request.form.values()]
 	final_features=[np.array(int_features)]
 	prediction = model.predict(final_features)
 	Detection =''
